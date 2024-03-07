8e88acd79b73f5051393314c69757f27c83ad4c2
Tulasi Lakshmi Majeti <tulasilakshmi.majeti@gmail.com>
2024-03-07 22:34:39 +0530

    Add project files.

---
+++ SystemDesigning/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GitProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            GitClass GitClass = new();
+            GitClass.GitMethod();
+        }
+    }
+    class GitClass
+    {
+        int GitID { get; set; }
+        string GitName { get; set; }
+
+        public void GitMethod()
+        {
+            Console.WriteLine("Git method");
+        }
+    }
+}
